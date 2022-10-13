ec7dfa2795638c6ea96d3e782de9cf4a65c8616b
Aun <aun@oneminutebookreview.com>
2022-10-14 00:17:54 +0100

    installed axios, added table and connected data

--- src/main/java/com/javareactcrud/fullstackbackend/controller/UserController.java
+++ src/main/java/com/javareactcrud/fullstackbackend/controller/UserController.java
@@ -2,14 +2,12 @@ package com.javareactcrud.fullstackbackend.controller;
 
 import com.javareactcrud.fullstackbackend.model.User;
 import com.javareactcrud.fullstackbackend.repository.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     private final UserRepository userRepository;
